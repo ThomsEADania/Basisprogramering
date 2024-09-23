0923e60f2318f5dbdc3d67422e5d03eaa848f712
ThomsEADania <thmo5@student.eadania.dk>
2024-09-23 20:07:14 +0200

    Hello World
    Prints "Hello World", "Write" (as a test of Console.Write), "Press any key to close the program" and waits for user input

--- Dag 1/Program.cs
+++ Dag 1/Program.cs
@@ -10,6 +10,17 @@ namespace Dag_1
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine adds a newline/line break after printing the string
+            Console.WriteLine("Hello World!");
+
+            //Console.Write does not add a newline/line break after printing the string
+            Console.Write("Write");
+
+            //Added for clarity, only prints string, does not add any functionality beyond that
+            Console.WriteLine("Press any key to close the program...");
+            
+            //Waits for user input before continuing (closing the program, as there isn't any more code)
+            Console.ReadLine();
         }
     }
 }
