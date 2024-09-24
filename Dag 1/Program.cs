3e90f4f4f5eac813327456c37996115d0346a94e
ThomsEADania <thmo5@student.eadania.dk>
2024-09-24 12:04:07 +0200

    Dag 1 - Øvelse 6
    Rykket øvelse 6 over i sit eget projekt og tilføjede lidt for klarhedens skyld

--- Dag 1/Program.cs
+++ Dag 1/Program.cs
@@ -10,22 +10,6 @@ namespace Dag_1
     {
         static void Main(string[] args)
         {  
-            //Øvelse 5 - Slide 79 (gul) - "Find ud af hvad forskellen er på ReadLine og ReadKey"
-
-            //ReadLine venter på at brugeren trykker Enter og "læser" alt den tekst man har indtastet og giver en string, bruges f.eks. til
-            //at lade brugeren indtaste deres navn
-
-            //ReadKey "læser" input med det samme en tast trykkes og giver en "ConsoleKeyInfo" som indeholder data om hvilken tast blev trykket
-            //& om der er nogle "modifier" keys som CTRL, SHIFT eller ALT
-
-
-
-            //Øvelse 6 - Slide 80 (rød) - "Lav et program der skriver indtastet tekst ud til skærmen, men uden brug af variabler"
-
-            Console.WriteLine(Console.ReadLine());
-
-
-
             //Øvelse 7 - Slide 86 (grøn) - "Lav et mailing label program, som spørger efter og printer: adresse, by, land, navn"
 
             //Øvelse 8 - Slide 87 (gul) - "Udvid programmet med telefonnummer, postnummer, alder etc."
@@ -39,11 +23,6 @@ namespace Dag_1
             //Øvelse 11 - Slide 97 (gul)
 
             //Øvelse 12 - Slide 98 (rød)
-            
-
-
-            //Waits for user input before continuing (closing the program, as there isn't any more code)
-            Console.ReadLine();
         }
     }
 }
--- Øvelse 6/Program.cs
+++ Øvelse 6/Program.cs
@@ -10,6 +10,16 @@ namespace Øvelse_6
     {
         static void Main(string[] args)
         {
+            //Øvelse 6 - Slide 80 (rød) - "Lav et program der skriver indtastet tekst ud til skærmen, men uden brug af variabler"
+
+            Console.WriteLine("Write anything and the console will print it :)");
+            Console.WriteLine(Console.ReadLine());
+
+
+
+            //Pauser programmet
+            Console.WriteLine("Press any key to close the program");
+            Console.ReadKey();
         }
     }
 }
