71c0bbf83b2c075f1b27af391ff4178670a04e4a
ThomsEADania <thmo5@student.eadania.dk>
2024-09-24 19:57:22 +0200

    Øvelse 10 kommentar
    Tilføjede opg besk til øvelse 10 (dag 2)

--- Dag 2 - Øvelse 10/Program.cs
+++ Dag 2 - Øvelse 10/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Slide 70 "Tæl fra 1 til 10 på 3 måder. Øvelsen skal laves på 3 måder med en while, do..while og en for-løkke"
+
 namespace Dag_2___Øvelse_10
 {
     internal class Program
