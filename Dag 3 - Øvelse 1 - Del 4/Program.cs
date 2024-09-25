a3e7a4ee0b82788f1fbd9830b2ab23cad0d07d47
ThomsEADania <thmo5@student.eadania.dk>
2024-09-25 11:42:41 +0200

    Øvelse 1 del 1-4 dag 3
    Klared øvelse 1, del 1-4 fra dag 3

---
+++ Dag 3 - Øvelse 1 - Del 4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Del 4 "Lav en enum til at definere en retning:
+//1. Lav enum og lav en variabel der bruger denne enum som datatype.
+//2. Skriv variablens værdi ud til skærmen.
+//3. Lav et program, der spørger hvilken retning spilleren vil bevæge sig i.
+// -> Når spilleren vælger en retning skal variablen ændres til den valgte retning.
+// -> F.eks.Tast 1 for north, 2 for south, 3 for east, 4 for west.
+//4. Variablens nye værdi skrives ud til skærmen."
+
+namespace Dag_3___Øvelse_1___Del_4
+{
+    enum direction: byte
+    {
+        North = 1, South = 2, East = 3, West = 4, Exit=5
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            direction myDirection = direction.North;
+
+            while (myDirection != direction.Exit)
+            {
+                Console.WriteLine("Venture forth traveller! Choose a direction to move:" +
+                    "\n 1. North" +
+                    "\n 2. South" +
+                    "\n 3. East" +
+                    "\n 4. West" +
+                    "\n 5. Exit");
+
+                myDirection = (direction)Enum.Parse(typeof(direction), Console.ReadLine());
+
+                Console.Clear();
+
+                Console.WriteLine($"You went {myDirection}.\n");
+            }
+        }
+    }
+}
---
+++ Dag 3 - Øvelse 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Slide 23
+
+namespace Dag_3___Øvelse_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Del 1 "Parse noget input fra en Readline() til en int værdi fx indtast 10 og gem det i, int myInput;"
+            Console.WriteLine("Input an integer:");
+            
+            int myInput;
+            myInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"myInput is {myInput}\n");
+
+
+
+            //Del 2 "Prøv at caste en int værdi som er højre end 255 og skriv resultatet ud til konsollen. Tilføj en checked block til jeres overflow cast"
+            byte myByte;
+
+            checked //Hvis checked-blokken ikke er her, kommer der et overflow, men ikke en fejl, checked laver en exception hvis der kommer et overflow 
+            {
+                myByte = (byte)myInput;
+            }
+
+            Console.WriteLine($"myByte is {myByte}\n");
+
+
+
+            //Del 3 "Prøv at caste en float værdi 2322.223 til en int og skriv resultatet ud til konsollen"
+            float myFloat = 2322.223F;
+            int myInt;
+
+            myInt = (int)myFloat;
+
+            Console.WriteLine($"Converting myFloat ({myFloat}) to myInt: \nmyInt is now {myInt}\n");
+
+            
+
+            //Pauser programmet
+            Console.WriteLine("Press any key to close the program...");
+            Console.ReadKey();
+        }
+    }
+}
