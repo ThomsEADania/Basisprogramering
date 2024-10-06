cbda4da5cb63a93d14b03a9902bdfd5bdb0238b1
ThomsEADania <thmo5@student.eadania.dk>
2024-10-06 18:59:50 +0200

    Obligatoriske opgaver dag 2 & 4
    Lavede obligatoriske opgaver fra dag 2 (sten-saks-papir) og dag 4 (quiz program)
    Omdøbte obligatoriske opgave fra dag 1 (gæt et tal)

---
+++ Quiz-program - Dag 4/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_program___Dag_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = new string[25]
+            {
+                "apple",
+                "tiger",
+                "chair",
+                "rocket",
+                "cloud",
+                "window",
+                "bridge",
+                "garden",
+                "banana",
+                "pirate",
+                "pencil",
+                "flower",
+                "island",
+                "puzzle",
+                "dragon",
+                "helmet",
+                "ladder",
+                "circle",
+                "bottle",
+                "candle",
+                "forest",
+                "giraffe",
+                "pyramid",
+                "shadow",
+                "dolphin"
+            };
+            
+            Random rand = new Random();
+
+            bool resetGame = true;
+
+            while (resetGame)
+            {
+                byte wordIndex = Convert.ToByte(rand.Next(25)); //Generates random number from 0 to 24 (25 is the exclusive upper limit)
+                string secretWord = words[wordIndex]; //Picks word from words array based on random number wordIndex
+
+                bool[] charGuessed = new bool[secretWord.Length]; //Creates boolean array with length equal to amount of characters in secretWord
+                byte guessesLeft = 10; //Sets amount of guesses
+                
+                StringBuilder wrongChars = new StringBuilder(); //Creates a stringbuilder to save all the wrong guesses
+                
+                bool gameRunning = true;
+
+                while (gameRunning)
+                {
+                    #region UI
+                    Console.Clear(); //Clears console before each "round"
+
+                    for (byte i = 0; i < secretWord.Length; i++) //Runs the same amount of times as amount of characters in secretWord
+                    {
+                        if (charGuessed[i] == true) //Displays char in secretWord at index i if the char has been guessed
+                        {
+                            Console.Write(secretWord[i]);
+                        }
+                        else //Displays _ if the char in secretWord at index i has not been guessed
+                        {
+                            Console.Write('_');
+                        }
+                    }
+
+                    Console.WriteLine($"   Guesses left: {guessesLeft}" + //Displays number of guesses left
+                        $"\nIncorrect guessed characters: {wrongChars}\n"); //Displays incorrect guessed characters
+                    #endregion
+
+                    #region Guessing
+                    Console.WriteLine("Enter a character to guess");
+
+                    char playerGuess;
+                    bool guessPrompt = true;
+
+                    while(guessPrompt == true) //Keeps prompting for guess until a valid input is entered
+                    {
+                        if (char.TryParse(Console.ReadLine(), out playerGuess)) //Take player input and checks if it can be parsed to a char
+                        {
+                            #region Checking guess
+                            guessPrompt = false; //If input can be parsed, set guessPrompt to false so it stops prompting for input
+
+                            bool correctChar = secretWord.Contains(playerGuess); //Checks if secretWord contains the guessed char (playerGuess)
+
+                            if (correctChar)
+                            {
+                                for (byte i = 0; i < secretWord.Length; i++) //Runs for each character of secretWord
+                                {
+                                    if (!charGuessed[i] == true) //Skips characters in the secretWord if they have already been guessed correctly
+                                    {
+                                        if (playerGuess == secretWord[i]) //Checks if guessed char matches char at index i in string secretWord
+                                        {
+                                            charGuessed[i] = true; //Set value in boolean array charGuess at index i to true
+                                        }
+                                    }
+                                }
+                            }
+                            else //If secretWord does not contain guessed char
+                            {
+                                guessesLeft--; //Decreases number of guesses left
+
+                                wrongChars.Append(playerGuess + " "); //Adds guessed char to wrongChars stringbuilder with a space
+                            }
+                            #endregion
+                        }
+                        else //If input can´t be parsed, displays error message
+                        {
+                            Console.WriteLine("Invalid input, try again");
+                        }
+                    }
+                    #endregion
+
+                    #region Checking if game is won
+                    bool secretWordGuessed = true;
+
+                    foreach (bool value in charGuessed) //Checks if all values in charGuessed bool array is true, if not the game continues
+                    {
+                        if (!value)
+                        {
+                            secretWordGuessed = false;
+                            break;
+                        }
+                    }
+                    #endregion
+
+                    #region Win/Lose message
+                    if (secretWordGuessed) //If the word was guessed
+                    {
+                        Console.WriteLine($"\nYou guessed the word! it was: {secretWord}");
+
+                        Console.WriteLine("\nPress any key to continue");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else if (guessesLeft <= 0) //If player runs out of guesses (didn´t guess the word)
+                    {
+                        Console.WriteLine($"\nYou lost, the secret word was: {secretWord}");
+
+                        Console.WriteLine("\nPress any key to continue");
+                        Console.ReadKey();
+                        break;
+                    }
+                    #endregion
+                }
+
+                #region Play again/Exit prompt
+                Console.Clear(); //Clears console so it is only the prompt on screen
+
+                bool exitPrompt = true;
+
+                while (exitPrompt) //Keeps prompting until a valid input is given
+                {
+                    Console.WriteLine("Do you want to play again or exit?");
+                    
+                    string answer = Console.ReadLine().ToLower();
+                    
+                    if (answer == "play again" || answer == "play")
+                    {
+                        exitPrompt = false;  
+                        resetGame = true;
+                    }
+                    else if (answer == "exit")
+                    {
+                        exitPrompt = false;
+                        resetGame = false;
+                    }
+                    else
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("Invalid input, try again\n");
+                    }
+                }
+                #endregion
+            }
+        }
+    }
+}
---
+++ Sten-saks-papir - Dag 2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sten_saks_papir___Dag_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepPlaying = true;
+
+            while (keepPlaying == true) //The "== true" is technically unneccesary for function, but adds readability
+            {
+                string playerInput; 
+                byte playerChoice = 0;
+                bool choicePrompt = true;
+                bool exit = false;
+
+                while (choicePrompt == true)
+                {
+                    #region UI
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine("You can at any point type \"exit\" or \"quit\" to close the program");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                    Console.WriteLine("What hand will you play:\n" +
+                        "1. Rock\n" +
+                        "2. Paper\n" +
+                        "3. Scissors\n");
+                    #endregion
+
+                    #region Player input
+                    playerInput = Console.ReadLine().ToLower(); //Take player input
+
+                    switch (playerInput) //"Convert" player input to byte or exit the program
+                    {
+                        case string n when (n == "1" || n == "rock"):
+                            playerChoice = 1;
+                            choicePrompt = false;
+                            break;
+
+                        case string n when (n == "2" || n == "paper"):
+                            playerChoice = 2;
+                            choicePrompt = false;
+                            break;
+
+                        case string n when (n == "3" || n == "scissors"):
+                            playerChoice = 3;
+                            choicePrompt = false;
+                            break;
+
+                        case string n when (n == "exit" || n == "quit"):
+                            choicePrompt = false;
+                            keepPlaying = false;
+                            exit = true;
+                            break;
+
+                        default: //Handles incorrect input
+                            Console.Clear();
+
+                            Console.WriteLine("Invalid input, try again\n");
+                            
+                            break;
+                    }
+                }
+
+                if (exit == true) //Breaks gameloop if player entered exit/quit
+                {
+                    break;
+                }
+                #endregion
+
+                //Computer choice - Assignment said to use switch, but it seems really unneccasary to me
+                Random rand = new Random();
+                byte npcChoice = Convert.ToByte(rand.Next(1, 4)); //Generates a byte between 1-3 (4 is max value and is excluded)
+                    
+                Console.Clear(); //Clears console to avoid cluttered interface
+
+                #region Display result
+                Console.WriteLine("Previous round result:"); //Technically not previous at this point in the code, but it is when playing the game
+
+                if (playerChoice == npcChoice) //Would have used a switch for this part, but the assignment said to use an if statement ¯\_(ツ)_/¯
+                {
+                    Console.WriteLine("Ended in draw");
+                }
+
+                else if (playerChoice == 1 && npcChoice == 3) //Player wins with rock
+                {
+                    Console.WriteLine("Player won with rock against scissors");
+                }
+
+                else if (playerChoice == 2 && npcChoice == 1) //Player wins with paper
+                {
+                    Console.WriteLine("Player won with paper against rock");
+                }
+
+                else if (playerChoice == 3 && npcChoice == 2) //Player wins with scissors
+                {
+                    Console.WriteLine("Player won with scissors against paper");
+                }
+
+                else if (playerChoice == 1 && npcChoice == 2) //Player loses with rock
+                {
+                    Console.WriteLine("Player lost with rock agaisnt paper");
+                }
+
+                else if (playerChoice == 2 && npcChoice == 3) //Player loses with paper
+                {
+                    Console.WriteLine("Player lost with paper against scissors");
+                }
+
+                else if (playerChoice == 3 && npcChoice == 1) //Player loses with scissors
+                {
+                    Console.WriteLine("Player lost with scissors against rock");
+                }
+
+                Console.WriteLine(); //Empty line for readability in-game
+                #endregion
+            }
+        }
+    }
+}
